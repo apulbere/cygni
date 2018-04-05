54184fce521ab5176e7ed48a22966ddf2717fde4
Adrian Pulbere <apulbere@yahoo.com>
2018-04-06 01:38:00 +0300

    fixes

--- src/main/scala/com/apulbere/cygni/util/QrCodeGenerator.scala
+++ src/main/scala/com/apulbere/cygni/util/QrCodeGenerator.scala
@@ -6,32 +6,29 @@ import com.google.zxing.{BarcodeFormat, EncodeHintType, MultiFormatWriter}
 
 import scala.collection.JavaConverters._
 
-private class QrCodeGenerator {
+class QrCodeGenerator {
+  private val multiFormatWriter = new MultiFormatWriter()
+  private val qrParams = Map(
+    EncodeHintType.ERROR_CORRECTION -> ErrorCorrectionLevel.L,
+    EncodeHintType.CHARACTER_SET -> "utf-8"
+  )
 
   def from(text: String): String = {
-    val qrParams = Map(
-      EncodeHintType.ERROR_CORRECTION -> ErrorCorrectionLevel.L,
-      EncodeHintType.CHARACTER_SET -> "utf-8"
-    )
-    val bitMatrix = new MultiFormatWriter().encode(text, BarcodeFormat.QR_CODE, 1, 1, qrParams.asJava)
+    val bitMatrix = multiFormatWriter.encode(text, BarcodeFormat.QR_CODE, 1, 1, qrParams.asJava)
     toAscii(bitMatrix)
   }
 
-  def toAscii(bitMatrix: BitMatrix): String = {
+  private def toAscii(bitMatrix: BitMatrix): String = {
     val sb = new StringBuilder
-    var rows = 0
-    while(rows < bitMatrix.getHeight) {
-      var cols = 0
-      while(cols < bitMatrix.getWidth) {
+    for(rows <- 0 until bitMatrix.getHeight) {
+      for(cols <- 0 until bitMatrix.getWidth) {
         if(!bitMatrix.get(rows, cols)) {
           sb.append("\u001b[47m  \u001b[0m") //[40m
         } else {
           sb.append("\u001b[40m  \u001b[0m") //[1m
         }
-        cols += 1; cols - 1
       }
       sb.append("\n")
-      rows += 1; rows - 1
     }
     sb.toString
   }
